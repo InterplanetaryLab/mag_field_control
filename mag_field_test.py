1936e5a98cb539bc73d82ed4164a90d96f036593
rpi-adcs <iilab@asu.edu>
2021-06-21 17:06:24 -0700

    added some more user input test code

--- mag_field_test.py
+++ mag_field_test.py
@@ -5,14 +5,23 @@ import time
 from mag_field_control import MAG_CONTROL
 
 
-test = MAG_CONTROL(0,22,23,24)
+def get_mag_user():
+    coil_dir = input(' Enter Coil Dir: 1 or 0 : ')
+    coil_mag = input(' Enter Coil mag: 0 to 255: ')
+    return [coil_dir,coil_mag]
+
+test = MAG_CONTROL(0,24,22,23)
+
+time_delta_sec = 20 # 10 seconds to wait to get to set point for now
 try:
     while True:
-        #test.set_coil_output_open(1,255)
-        test.print_coil_state()
-        
-        time.sleep(.15) 
-        test.print_mag_output()
+        user_out = get_mag_user()
+        test.set_coil_output_open(user_out[0],user_out[1])
+        last_time = time.time()
+        while int(last_time%60)+time_delta_sec > int(time.time()%60):
+            test.print_coil_state() 
+            time.sleep(.15) 
+            test.print_mag_output()
 
 except KeyboardInterrupt:
     test.stop_coil()
